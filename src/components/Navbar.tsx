fa0f9c0b7651b0f048fe3e0ac4b637bc8f6a0f58
Kelechi Otugbuali <hilakechi@gmail.com>
2023-07-27 15:51:09 +0100

    NavData created in the index file in constants folder for Navbar reuseability

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React from "react";
+import { NavData } from "../constants";
 
 const Navbar = () => {
   return (
-    <div className='w-full bg-[#111111] h-20 flex'>
-
-        <ul className='flex items-center justify-center mx-auto gap-8 text-[#ffcb74] text-2xl '>
-            <li>Balance: <span className='text-white'>XXX</span></li>
-            <li>Bet: <span className='text-white'>XXX</span></li>
-            <li>Win: <span className='text-white'>XXX</span></li>
-        </ul>
+    <div className="w-full bg-[#111111] h-20 flex">
+      <ul className="flex items-center justify-center mx-auto gap-8 text-[#ffcb74] text-2xl ">
+        {NavData.map((item) => (
+          <li key={item.title} className=" uppercase">
+            {item.title}: <span className="text-white">{item.value}</span>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
---
+++ src/constants/index.ts
@@ -0,0 +1,14 @@
+export const NavData =[
+    {
+    title: 'Balance', value:'5000'
+},
+{
+    title: 'Bet', value:'500'
+},
+{
+    title: 'Win', value:''
+},
+
+
+
+]
