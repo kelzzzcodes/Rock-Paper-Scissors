f8e8d4a3192637e8d5dc27e04a893131658bec56
Kelechi Otugbuali <hilakechi@gmail.com>
2023-08-03 09:07:40 +0100

    Rock vs Paper section build

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -4,11 +4,23 @@ import { customCardsData } from "../constants";
 
 const GamePage = () => {
   return (
-    <div className="w-full flex h-[calc(100vh-5rem)]">
-      <div className="flex flex-col gap-10 justify-center items-center mx-auto justify-items-center">
-        <h2 className="flex capitalize text-xl  text-primary">
+    <div className="w-full flex flex-col h-[calc(100vh-5rem)] justify-center ">
+
+        <div className="flex justify-center mb-32 ">
+        <div className=" flex gap-10  items-baseline">
+          <p className="text-5xl font-bold uppercase">Rock</p>
+          <span className="text-primary text-2xl font-bold">VS</span>
+          <p className="text-5xl font-bold uppercase" >Paper</p>
+
+        </div>
+        </div>
+      <div className="flex flex-col gap-10 justify-center items-center  justify-items-center">
+
+      <h2 className="flex capitalize text-xl  text-primary">
           Pick your Positions
         </h2>
+
+
         <div className="flex flex-wrap items-center justify-center gap-4">
           {customCardsData.map((cardData) => (
             <CustomCard
